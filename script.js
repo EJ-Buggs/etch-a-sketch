bdd1dc3af0883d004ee7c4fcd667c91fff73d02b
ebuggs <ebuggs212@gmail.com>
2023-05-06 02:41:48 -0400

    add container and grid

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
 const grid = document.querySelector(".grid");
-let size = 10;
+let size = 256;
 let isDragging = false;
 
 for (let i = 0; i < size; i++) {
   const square = document.createElement("div");
-  square.style.width = "50px";
-  square.style.height = "50px";
-  square.style.backgroundColor = "red";
+  square.style.width = "100%";
+  square.style.height = "100%";
+  square.style.backgroundColor = "white";
 
   document.addEventListener("mousedown", function () {
     isDragging = true;
   });
 
   square.addEventListener("mousemove", function () {
-    isDragging ? (square.style.backgroundColor = "blue") : null;
+    isDragging ? (square.style.backgroundColor = "black") : null;
+  });
+
+  square.addEventListener("click", function () {
+    square.style.backgroundColor = "black";
   });
 
   document.addEventListener("mouseup", function () {
@@ -22,3 +26,7 @@ for (let i = 0; i < size; i++) {
 
   grid.appendChild(square);
 }
+
+grid.style.display = "grid";
+grid.style.gridTemplateColumns = "repeat(16, 1fr)";
+grid.style.gridTemplateRows = "repeat(16, 1fr)";
